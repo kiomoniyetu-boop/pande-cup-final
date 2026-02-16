61d70fa6988e4d5923c2d5539e58b648f4596a51
kiomoniyetu-boop <kiomoniyetu@gmail.com>
2026-02-16 13:20:01 +0300

    Update RegisterTeam.js

--- src/pages/RegisterTeam.js
+++ src/pages/RegisterTeam.js
@@ -8,16 +8,16 @@ import {
 const LOGO_PATH = "https://images.ctfassets.net/ax6wvfd84net/1T4feibK8k9Ft9Y6MdQul0/2807bebb7fbdf78ba3ea0d7e7bb5c71e/logo.png";
 
 const LOCATIONS = [
-  { group: 'KIOMONI', areas: ['Kiomoni', 'Mpirani', 'Mabayani', 'Ndumi', 'Mapande "A"', 'Kivuleni', 'Cross Z', 'Mbogo', 'Kilimanjaro', 'Mowe', 'Masaini', 'Muheza', 'Kikuluni', 'Chote', 'Lwande', 'Magubeni', 'Marembwe', 'Mavumbi', 'Mijohoroni', 'Mjesani', 'Mnyenzani'] },
+  { group: 'KIOMONI', areas: ['Kiomoni', 'Mpirani', 'Mabayani', 'Ndumi', 'Mapande "A"', 'Kivuleni', 'Cross Z', 'Mbogo', 'Kilimanjaro', 'Mowe', 'Masaini', 'Muheza', 'Kikuluni', 'Chote', 'Lwande', 'Magubeni', 'Marembwe', 'Mavumbi', 'Mijohoroni', 'Mjesani', 'Mnyenzani', 'Amboni', 'Mabokweni'] },
   { group: 'TANGA MJINI', areas: ['Ngamiani', 'Majengo', 'Mabawa', 'Central', 'Makorora', 'Mzingani', 'Tanga Sisi', 'Mnyanjani', 'Usagara', 'Nguvumali', 'Pongwe', 'Magaoni', 'Duga', 'Maweni', 'Chongoleani', 'Raskazone', 'Donge', 'Gofu'] },
   { group: 'DAR ES SALAAM', areas: ['Goba', 'Madale', 'Makongo', 'Mbezi Mwisho', 'Mbezi Beach', 'Kimara', 'Sinza', 'Ubungo', 'Mabibo', 'Tegeta', 'Bunju', 'Kinondoni', 'Ilala', 'Chamazi', 'Kijamboni', 'Mbagala', 'Boko', 'Kawe'] },
   { group: 'OUTSIDE', areas: ['Nje ya Tanga/Dar'] }
 ];
 
-const DAR_AREAS = ['Goba', 'Madale', 'Makongo', 'Mbezi Mwisho', 'Mbezi Beach', 'Kimara', 'Sinza', 'Ubungo', 'Mabibo', 'Tegeta', 'Bunju', 'Kinondoni', 'Ilala', 'Chamazi', 'Kijamboni', 'Mbagala', 'Boko', 'Kawe'];
+const DAR_AREAS = ['Goba', 'Madale', 'Makongo', 'Mbezi Mwisho', 'Mbezi Beach', 'Kimara', 'Sinza', 'Ubungo', 'Mabibo', 'Tegeta', 'Bunju', 'Kinondoni', 'Ilala', 'Chamazi', 'Kijamboni', 'Mbagala', 'Boko', 'Kawe', 'Msumi', 'Kwa Robert', 'Mkuranga', 'Kibaha', 'Pwani Mjini', 'Gongo La Mboto', 'Chanika'];
 
-const MIXX_LIPA_NAMBA = '15744793';
-const RECEIVER_NAME = 'FUTURE VISION';
+const MIXX_LIPA_NAMBA = '43852599';
+const RECEIVER_NAME = 'FESTO HENRY MSANGAWALE';
 const REGISTRATION_FEE = 70000;
 
 export default function RegisterTeam() {
